baced5a686538dbc60da3c1fddb11910d8a98e92
unknown <ria17041998@gmail.com>
2020-08-17 18:08:48 +0530

    todo delete and edit

--- main.js
+++ main.js
@@ -1,9 +1,59 @@
-var todos =[];
-var todo_list_wrapper=document.querySelector('.show_todo');
-function updateTodolist(todos){
-    if(todos.length==0)
-    {
-        todo_list_wrapper.textContent="No Todos";
-    }
+var todos = [{
+    id: 1,
+    title: "meeting",
+    desc: "meeting at 6pm with VC"
+},
+{
+    id: 2,
+    title: "shopping",
+    desc: "shopping at 7pm"
+}]; //entry array
+
+var show_todo = document.querySelector('.show_todo');
+
+if (todos.length == 0) {
+    show_todo.textContent = "No Todos";
 }
-updateTodolist(todos);
+else {
+    todos.forEach(function (todo) {
+        var todo_wrapper = document.createElement('div');
+
+        var wrapper_div = document.createElement('div');
+        var action_wrapper = document.createElement('div');
+
+        var title = document.createElement('h3');
+        var desc = document.createElement('h5');
+
+        title.textContent = todo.title;
+        desc.textContent = todo.desc;
+
+        var deleteButton = document.createElement('button');
+        var editButton = document.createElement('button');
+
+        deleteButton.addEventListener('click', function () {
+            console.log(todo.id);
+            //deleteTodo
+        });
+
+        editButton.addEventListener('click', function () {
+            console.log(todo.id);
+        });
+
+        deleteButton.textContent = 'Delete';
+        editButton.textContent = 'Edit';
+
+        wrapper_div.appendChild(title);
+        wrapper_div.appendChild(desc);
+
+        action_wrapper.appendChild(deleteButton);
+        action_wrapper.appendChild(editButton);
+
+        todo_wrapper.appendChild(wrapper_div);
+        todo_wrapper.appendChild(action_wrapper);
+
+        show_todo.appendChild(todo_wrapper);
+
+    });
+}
+
+
